c49658ca8b971d5db51beb55ff801aec1053933e
eliveltonsf <elivelton.seven@gmail.com>
2020-12-09 06:56:06 -0300

    add Dashboard Page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
 import ContentHeader from '../../components/ContentHeader'
+import SelectInput from '../../components/SelectInput';
 
 import { Container } from './styles'
 
 
 const Dashboard: React.FC = () => {
+
+  const options = [
+    { value: 'Elivelton', label: 'Rodrigo' },
+    { value: 'Maria', label: 'Maria' },
+    { value: 'Ana', label: 'Ana' }
+  ]
+
   return (
     <Container>
-      <ContentHeader></ContentHeader>
+      <ContentHeader title="Dashboard" lineColor="#f7931b">
+        <SelectInput options={options} />
+
+      </ContentHeader>
     </Container>
   )
 }
