7d4e8d3fcd243c3c1ba0ffd027b4672d7d665ee6
eliveltonsf <elivelton.seven@gmail.com>
2020-12-09 06:55:54 -0300

    add List Page

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import { Container } from './styles'
+
+import ContentHeader from '../../components/ContentHeader'
+import SelectInput from '../../components/SelectInput';
+
 const List: React.FC = () => {
+
+  const options = [
+    { value: 'Elivelton', label: 'Rodrigo' },
+    { value: 'Maria', label: 'Maria' },
+    { value: 'Ana', label: 'Ana' }
+  ]
+
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      <ContentHeader title="Saídas" lineColor="#e44c4e">
+        <SelectInput options={options} />
+
+      </ContentHeader>
+    </Container>
   )
 }
 
